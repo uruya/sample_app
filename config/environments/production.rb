05c7aeff21b5d79c7d389e84cefd5aa57f545152
Ubuntu <ubuntu@ip-172-31-40-99.ap-northeast-1.compute.internal>
2021-08-20 12:05:45 +0000

    production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,16 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sleepy-temple-5665.herokuapp.com'
+  host = 'https://sleepy-temple-56650.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port   => '587',
-    :address    => 'smtp.mailgun.org',
-    :user_name    => ENV['MAILGUN_SMTP_LOGIN'],
-    :password   => ENV['MAILGUN_SMTP_PASSWORD'],
+    :address    => 'smtp.sendgrid.net',
+    :user_name    => ENV['SENDGRID_USERNAME'],
+    :password   => ENV['SENDGRID_PASSWORD'],
     :domain   => 'heroku.com',
     :authentication   => :plain,
+    :enable_starttls_auto  => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
