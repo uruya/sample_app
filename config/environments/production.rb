a0e69b3d410a5a9b7f6c44a6b0d2c185b55cbf32
uruya <1ayawolf1@gmail.com>
2021-08-20 12:24:41 +0000

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,12 +71,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port   => '587',
-    :address    => 'smtp.sendgrid.net',
-    :user_name    => ENV['SENDGRID_USERNAME'],
-    :password   => ENV['SENDGRID_PASSWORD'],
-    :domain   => 'heroku.com',
+    :address    => 'smtp.mailgun.org',
+    :user_name    => ENV['MAILGUN_SMTP_LOGIN'],
+    :password   => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain   => host,
     :authentication   => :plain,
-    :enable_starttls_auto  => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
