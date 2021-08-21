593fdd5b81acd98f58e2c653fd2ca4e5914bbe69
uruya <1ayawolf1@gmail.com>
2021-08-21 07:47:01 +0000

    production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://sleepy-temple-56650.herokuapp.com/'
+  host = 'https://sleepy-temple-56650.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+     :port => ENV['MAILGUN_SMTP_PORT'],
+     :address => ENV['MAILGUN_SMTP_SERVER'],
+     :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+     :password => ENV['MAILGUN_SMTP_PASSWORD'],
+     :domain => host,
+     :authentication => :plain,
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
