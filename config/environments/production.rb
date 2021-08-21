3dfdbcfd2f1d5a53e80fd3a4493f6f5b947b6f71
uruya <1ayawolf1@gmail.com>
2021-08-21 05:40:26 +0000

    production.rb fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,12 +70,12 @@ Rails.application.configure do
   host = 'https://sleepy-temple-56650.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :port   => '587',
-    :address    => 'smtp.mailgun.org',
-    :user_name    => ENV['MAILGUN_SMTP_LOGIN'],
-    :password   => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain   => host,
-    :authentication   => :plain,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
